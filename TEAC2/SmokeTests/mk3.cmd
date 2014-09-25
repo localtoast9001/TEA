f38f297963685810e7b5b769556903547274381a
jrowlett <motherplucker@gmail.com>
2014-09-24 19:57:09 -0700

    Address and Dereference Expressions

---
+++ TEAC2/SmokeTests/mk3.cmd
@@ -0,0 +1,4 @@
+..\..\bin\TEAC.exe /FaTest3.asm Test3.tea
+ml /c /Cp /Cx /Zi /Fotest3.obj Test3.asm
+REM ml /c /Cp /Cx /Zi /Fotest1Main.obj Test1Main.asm
+REM link /SUBSYSTEM:CONSOLE /DEBUG /OUT:Test1.exe /PDB:Test1.pdb Test1.obj Test1Main.obj msvcrt.lib
