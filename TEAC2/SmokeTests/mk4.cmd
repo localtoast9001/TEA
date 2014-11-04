3c2072816cdf16b3326ce8e138fd24c7b242656b
jrowlett <motherplucker@gmail.com>
2014-11-04 12:41:53 -0800

    New test case.

---
+++ TEAC2/SmokeTests/mk4.cmd
@@ -0,0 +1,4 @@
+..\..\bin\TEAC.exe /FaTest4.asm Test4.tea
+ml /c /Cp /Cx /Zi /Fotest4.obj Test4.asm
+REM ml /c /Cp /Cx /Zi /Fotest1Main.obj Test1Main.asm
+REM link /SUBSYSTEM:CONSOLE /DEBUG /OUT:Test1.exe /PDB:Test1.pdb Test1.obj Test1Main.obj msvcrt.lib
