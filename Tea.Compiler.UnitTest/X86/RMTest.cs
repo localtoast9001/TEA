195efc829eb55b1955e2ff9f2f29350ab148ae92
localtoast9001 <motherplucker@gmail.com>
2022-12-15 21:45:48 -0800

    Fix building on Windows and Clean target

--- Tea.Compiler.UnitTest/X86/RMTest.cs
+++ Tea.Compiler.UnitTest/X86/RMTest.cs
@@ -25,14 +25,14 @@ namespace Tea.Compiler.UnitTest.X86
             Tuple<RM, string>[] testCases = new Tuple<RM, string>[]
             {
                 new Tuple<RM, string>(RM.FromRegister(Register.EAX), "eax"),
-                new Tuple<RM, string>(RM.Address(Register.EAX), "[eax]"),
+                new Tuple<RM, string>(RM.Address(Register.EAX), "dword ptr [eax]"),
                 new Tuple<RM, string>(RM.Address(Register.EAX, 8), "qword ptr [eax]"),
                 new Tuple<RM, string>(RM.Address(Register.EAX, 10), "tword ptr [eax]"),
-                new Tuple<RM, string>(RM.Address(Register.ESP), "[esp]"),
-                new Tuple<RM, string>(RM.Address("label1"), "[label1]"),
-                new Tuple<RM, string>(RM.Address(Register.EBP, -4, "_x$"), "_x$[ebp]"),
-                new Tuple<RM, string>(RM.Address(Register.EBP, -4, null), "-4[ebp]"),
-                new Tuple<RM, string>(RM.Address(Register.ESP, 4, null), "4[esp]"),
+                new Tuple<RM, string>(RM.Address(Register.ESP), "dword ptr [esp]"),
+                new Tuple<RM, string>(RM.Address("label1"), "dword ptr [label1]"),
+                new Tuple<RM, string>(RM.Address(Register.EBP, -4, "_x$"), "dword ptr _x$[ebp]"),
+                new Tuple<RM, string>(RM.Address(Register.EBP, -4, null), "dword ptr -4[ebp]"),
+                new Tuple<RM, string>(RM.Address(Register.ESP, 4, null), "dword ptr 4[esp]"),
             };
 
             foreach (var testCase in testCases)
