e10a8ac4520b0b793ddf327fdd541857450b8bb1
Jon Rowlett <motherplucker@gmail.com>
2024-04-28 17:53:34 -0700

    Update .Net version. Fix open() bug.

--- Tea.Compiler.Binary.UnitTest/Usings.cs
+++ Tea.Compiler.Binary.UnitTest/Usings.cs
@@ -1,1 +1,7 @@
+// <copyright file="Usings.cs" company="Jon Rowlett">
+// Copyright (C) Jon Rowlett. All rights reserved.
+// </copyright>
+
+#pragma warning disable SA1200 // Using directives should be placed correctly
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+#pragma warning restore SA1200 // Using directives should be placed correctly
